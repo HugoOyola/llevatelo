00df011fee0a795384022690bb832035fad00ba9
Hugo Oyola <hugo.ham@outlook.com>
2025-08-03 01:35:35 -0500

    feat: eliminar la archivos no utilizados

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,4 @@
 export { default as HomeScreen } from './main/HomeScreen';
 export { default as CategoriesScreen } from './categories/CategoriesScreen';
 export { default as SearchScreen } from './search/SearchScreen';
-export { default as FavoritesScreen } from './favorites/FavoritesScreen';
 export { default as ProfileScreen } from './profile/ProfileScreen';
